e2c04e4e2736d62d978ae2196042fc3d810222bc
Manas Shukla <Manas Shukla>
2016-08-15 20:57:46 +0530

    Added LinkedIN and StumbleUpon

--- Test.java
+++ Test.java
@@ -9,34 +9,71 @@ public class Test {
 	/* Main Function */
 	public static void main(String[] args) throws IOException {
 
-		String PageURL = input();
-		Document doc = Jsoup.connect(PageURL).ignoreContentType(true).get();
-		String PageDoc = doc.toString();
-		System.out.println("Number of Likes : ");
-		printLikes(PageDoc);
-	}
+		System.out.println("1.Facebook 2.LinkedIN");
+		Scanner In = new Scanner(System.in);
+		int choice = In.nextInt();
+		Site(choice);
 
+	}
+	
+	static void Site(int choice){
+		
+		switch(choice)
+		{
+		case 1 : inputFB();
+		case 2 : inputLinkedIN();
+		}
+	}
 	/*
 	 * This function takes a URL of a page as input and inserts it into the
 	 * endpoint link
 	 */
-	static String input() {
+	static void inputFB() {
 
 		Scanner b = new Scanner(System.in);
 		System.out.println("URL of page");
 		String PageURL = b.nextLine();
 		String url = ("https://api.facebook.com/method/links.getStats?urls=%%" + PageURL + "%%&format=json");
-		return url;
+		Document doc = Jsoup.connect(PageURL).ignoreContentType(true).get();
+		String PageDoc = doc.toString();
+		System.out.println("Number of Likes : ");
+		printFBLikes(PageDoc);
+	}
+	
+	static void inputLinkedIN() {
+
+		Scanner b = new Scanner(System.in);
+		System.out.println("URL of page");
+		String PageURL = b.nextLine();
+		String url = ("https://www.linkedin.com/countserv/count/share?format=json&url=" + PageURL);
+		Document doc = Jsoup.connect(PageURL).ignoreContentType(true).get();
+		String PageDoc = doc.toString();
+		System.out.println("Number of Likes : ");
+		printLinkedIN(PageDoc);
 	}
 
 	/*
 	 * This function takes the whole Document of the endpoint link as an input
 	 * with data type String and prints out the number of likes on the page.
 	 */
-	static void printLikes(String Doc) {
+	static void printFBLikes(String Doc) {
 
 		int count = 0, i = 0;
 		while (count < 6) {
+			if (Doc.charAt(i) == ':') {
+				count++;
+			}
+			i++;
+		}
+		count = 0;
+		while (Doc.charAt(i) != ',') {
+			System.out.print(a.charAt(i++));
+		}
+	}
+	static void printLinkedIN(String Doc) {
+
+		int count = 0, i = 0;
+		while (count < 1) {
 			if (Doc.charAt(i) == ':') {
 				count++;
 			}
---
+++ stats.java
@@ -0,0 +1,92 @@
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.Scanner;
+
+public class stats {
+
+	/* Main Function */
+	public static void main(String[] args) throws IOException {
+		
+		int choice;
+		do {
+			System.out.println("\n1.Facebook \n2.LinkedIN\n3.Stumbled Upon\n4.Exit");
+			Scanner In = new Scanner(System.in);
+			choice = In.nextInt();
+			Site(choice);
+		} while (choice != 4);
+
+	}
+
+	static void Site(int choice) throws IOException {
+
+		switch (choice) {
+		case 1:
+			String PageDoc = input();
+			FBorS(PageDoc, 1);
+			break;
+		case 2:
+			PageDoc = input();
+			FBorS(PageDoc, 3);
+			break;
+		case 3:
+			PageDoc = input();
+			FBorS(PageDoc, 2);
+			break;
+		}
+	}
+
+	/*
+	 * This function takes URL of a page as input and returns it
+	 */
+	static String input() throws IOException {
+
+		Scanner b = new Scanner(System.in);
+		System.out.println("URL of page");
+		String PageURL = b.nextLine();
+		return PageURL;
+	}
+
+	/*
+	 * This function takes the whole Document of the endpoint link as an input
+	 * with data type String and calls the print function
+	 */
+	static void FBorS(String Doc, int ForS) throws IOException {
+		String url = ",";
+		if (ForS == 1) {
+			url = ("https://api.facebook.com/method/links.getStats?urls=%%" + Doc + "%%&format=json");
+			System.out.println("Number of Likes : ");
+		}
+		if (ForS == 2) {
+			url = ("http://www.stumbleupon.com/services/1.01/badge.getinfo?url=" + Doc);
+			System.out.println("Number of Views : ");
+		}
+		int coloncount = 6;
+		if (ForS == 3) {
+			url = ("https://www.linkedin.com/countserv/count/share?format=json&url=" + Doc);
+			coloncount = 1;
+		}
+		Document source = Jsoup.connect(url).ignoreContentType(true).get();
+		String PageDoc = source.toString();
+		Print(PageDoc, coloncount);
+	}
+
+	/* This function prints the stats of the entered URL */
+	static void Print(String PageDoc, int fcount) {
+
+		int count = 0, i = 0;
+		while (count < fcount) {
+			if (PageDoc.charAt(i) == ':') {
+				count++;
+			}
+			i++;
+		}
+		count = 0;
+		while (PageDoc.charAt(i) != ',') {
+			System.out.print(PageDoc.charAt(i++));
+		}
+	}
+
+}
